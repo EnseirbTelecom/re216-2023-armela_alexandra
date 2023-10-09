d24bf3c91e1ab5f06bf3c26606c0ddbd34763528
MONY ALEXANDRA <amony001@enseirb-matmeca.fr>
2023-10-09 16:24:11 +0200

    Req1.1&Req1.2

--- sample-jalon1/alexouche/client.c
+++ sample-jalon1/alexouche/client.c
@@ -1,72 +1,98 @@
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <unistd.h>
+#include <arpa/inet.h>  // Inclut l'en-tête pour les fonctions liées à inet (Internet).
+#include <netdb.h>      // Inclut l'en-tête pour les fonctions liées aux informations réseau.
+#include <netinet/in.h> // Inclut l'en-tête pour les structures et constantes Internet.
+#include <stdio.h>      // Inclut l'en-tête standard pour les entrées/sorties.
+#include <stdlib.h>     // Inclut l'en-tête standard pour les fonctions de gestion de la mémoire.
+#include <string.h>     // Inclut l'en-tête standard pour les fonctions de manipulation de chaînes de caractères.
+#include <sys/socket.h> // Inclut l'en-tête pour les fonctions liées aux sockets.
+#include <unistd.h>     // Inclut l'en-tête pour les fonctions liées aux appels système.
 
-#include "common.h"
+#include "common.h"     // Inclut l'en-tête personnalisé "common.h".
 
+// Définition de la fonction echo_client prenant un descripteur de socket en argument.
 void echo_client(int sockfd) {
-	char buff[MSG_LEN];
-	int n;
-	while (1) {
-		// Cleaning memory
-		memset(buff, 0, MSG_LEN);
-		// Getting message from client
-		printf("Message: ");
-		n = 0;
-		while ((buff[n++] = getchar()) != '\n') {} // trailing '\n' will be sent
-		// Sending message (ECHO)
-		if (send(sockfd, buff, strlen(buff), 0) <= 0) {
-			break;
-		}
-		printf("Message sent!\n");
-		// Cleaning memory
-		memset(buff, 0, MSG_LEN);
-		// Receiving message
-		if (recv(sockfd, buff, MSG_LEN, 0) <= 0) {
-			break;
-		}
-		printf("Received: %s", buff);
-	}
+    char buff[MSG_LEN]; // Déclare un tableau de caractères pour stocker les messages.
+    int n;
+
+    while (1) { // Boucle infinie pour la communication.
+        // Cleaning memory
+        memset(buff, 0, MSG_LEN); // Efface la mémoire tampon des messages.
+
+        // Getting message from client
+        printf("Message: "); // Affiche un message à l'utilisateur.
+        n = 0;
+        while ((buff[n++] = getchar()) != '\n') {} // Lit les caractères entrés par l'utilisateur jusqu'à '\n'.
+
+        // Sending message (ECHO)
+        if (send(sockfd, buff, strlen(buff), 0) <= 0) { // Envoie le message sur le socket.
+            break; // Sort de la boucle si l'envoi échoue.
+        }
+        printf("Message sent!\n"); // Affiche un message de confirmation d'envoi.
+
+        // Cleaning memory
+        memset(buff, 0, MSG_LEN); // Efface à nouveau la mémoire tampon des messages.
+
+        // Receiving message
+        if (recv(sockfd, buff, MSG_LEN, 0) <= 0) { // Reçoit un message du serveur.
+            break; // Sort de la boucle si la réception échoue.
+        }
+        printf("Received: %s", buff); // Affiche le message reçu.
+    }
 }
 
-int handle_connect() {
-	struct addrinfo hints, *result, *rp;
-	int sfd;
-	memset(&hints, 0, sizeof(struct addrinfo));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	if (getaddrinfo(SERV_ADDR, SERV_PORT, &hints, &result) != 0) {
-		perror("getaddrinfo()");
-		exit(EXIT_FAILURE);
-	}
-	for (rp = result; rp != NULL; rp = rp->ai_next) {
-		sfd = socket(rp->ai_family, rp->ai_socktype,rp->ai_protocol);
-		if (sfd == -1) {
-			continue;
-		}
-		if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1) {
-			break;
-		}
-		close(sfd);
-	}
-	if (rp == NULL) {
-		fprintf(stderr, "Could not connect\n");
-		exit(EXIT_FAILURE);
-	}
-	freeaddrinfo(result);
-	return sfd;
+// Fonction pour gérer la connexion au serveur.
+int handle_connect(char *server_name,char *server_port) {
+    struct addrinfo hints, *result, *rp;
+    int sfd;
+
+    // Initialise la structure hints à zéro et configure les paramètres de la connexion.
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC; // Indique que l'on peut utiliser IPv4 ou IPv6.
+    hints.ai_socktype = SOCK_STREAM; // Indique que l'on utilise un socket de type TCP.
+
+    // Obtient les informations sur l'hôte distant (adresse IP et port).
+    if (getaddrinfo(server_name, server_port, &hints, &result) != 0) {
+        perror("getaddrinfo()"); // Affiche un message d'erreur en cas d'échec.
+        exit(EXIT_FAILURE); // Quitte le programme en cas d'erreur.
+    }
+
+    // Parcourt la liste des résultats obtenus à partir de getaddrinfo.
+    for (rp = result; rp != NULL; rp = rp->ai_next) {
+        sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if (sfd == -1) {
+            continue; // Continue la boucle si la création du socket échoue.
+        }
+        if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1) {
+            break; // Établit la connexion avec le serveur et sort de la boucle.
+        }
+        close(sfd); // Ferme le socket si la connexion échoue.
+    }
+
+    // Vérifie si rp est toujours NULL (connexion échouée).
+    if (rp == NULL) {
+        fprintf(stderr, "Could not connect\n"); // Affiche un message d'erreur.
+        exit(EXIT_FAILURE); // Quitte le programme en cas d'échec de la connexion.
+    }
+
+    freeaddrinfo(result); // Libère la mémoire utilisée par les informations obtenues.
+
+    return sfd; // Retourne le descripteur de socket connecté au serveur.
 }
 
-int main() {
-	int sfd;
-	sfd = handle_connect();
-	echo_client(sfd);
-	close(sfd);
-	return EXIT_SUCCESS;
-}
 
+int main(int argc, char *argv[]) {
+	if (argc != 3) {
+        fprintf(stderr, "Usage: %s <server_name> <server_port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    char *server_name = argv[1];
+    char *server_port = argv[2];
+
+    int sfd;
+
+    sfd = handle_connect(server_name,server_port); // Établit la connexion au serveur.
+    echo_client(sfd); // Lance la fonction de communication client-serveur.
+    close(sfd); // Ferme le socket.
+    return EXIT_SUCCESS;
+}
--- sample-jalon1/alexouche/server.c
+++ sample-jalon1/alexouche/server.c
@@ -1,78 +1,97 @@
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <unistd.h>
+#include <arpa/inet.h>  // Inclut l'en-tête pour les fonctions liées à inet (Internet).
+#include <netdb.h>      // Inclut l'en-tête pour les fonctions liées aux informations réseau.
+#include <netinet/in.h> // Inclut l'en-tête pour les structures et constantes Internet.
+#include <stdio.h>      // Inclut l'en-tête standard pour les entrées/sorties.
+#include <stdlib.h>     // Inclut l'en-tête standard pour les fonctions de gestion de la mémoire.
+#include <string.h>     // Inclut l'en-tête standard pour les fonctions de manipulation de chaînes de caractères.
+#include <sys/socket.h> // Inclut l'en-tête pour les fonctions liées aux sockets.
+#include <unistd.h>     // Inclut l'en-tête pour les fonctions liées aux appels système.
 
-#include "common.h"
+#include "common.h"     // Inclut l'en-tête personnalisé "common.h".
 
+// Définition de la fonction echo_server prenant un descripteur de socket en argument.
 void echo_server(int sockfd) {
-	char buff[MSG_LEN];
-	while (1) {
-		// Cleaning memory
-		memset(buff, 0, MSG_LEN);
-		// Receiving message
-		if (recv(sockfd, buff, MSG_LEN, 0) <= 0) {
-			break;
-		}
-		printf("Received: %s", buff);
-		// Sending message (ECHO)
-		if (send(sockfd, buff, strlen(buff), 0) <= 0) {
-			break;
-		}
-		printf("Message sent!\n");
-	}
+    char buff[MSG_LEN]; // Déclare un tableau de caractères pour stocker les messages.
+
+    while (1) { // Boucle infinie pour la communication.
+        // Cleaning memory
+        memset(buff, 0, MSG_LEN); // Efface la mémoire tampon des messages.
+
+        // Receiving message
+        if (recv(sockfd, buff, MSG_LEN, 0) <= 0) { // Reçoit un message du client.
+            break; // Sort de la boucle si la réception échoue.
+        }
+        printf("Received: %s", buff); // Affiche le message reçu.
+
+        // Sending message (ECHO)
+        if (send(sockfd, buff, strlen(buff), 0) <= 0) { // Envoie le message sur le socket.
+            break; // Sort de la boucle si l'envoi échoue.
+        }
+        printf("Message sent!\n"); // Affiche un message de confirmation d'envoi.
+    }
 }
 
-int handle_bind() {
-	struct addrinfo hints, *result, *rp;
-	int sfd;
-	memset(&hints, 0, sizeof(struct addrinfo));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;
-	if (getaddrinfo(NULL, SERV_PORT, &hints, &result) != 0) {
-		perror("getaddrinfo()");
-		exit(EXIT_FAILURE);
-	}
-	for (rp = result; rp != NULL; rp = rp->ai_next) {
-		sfd = socket(rp->ai_family, rp->ai_socktype,
-		rp->ai_protocol);
-		if (sfd == -1) {
-			continue;
-		}
-		if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0) {
-			break;
-		}
-		close(sfd);
-	}
-	if (rp == NULL) {
-		fprintf(stderr, "Could not bind\n");
-		exit(EXIT_FAILURE);
-	}
-	freeaddrinfo(result);
-	return sfd;
+// Fonction pour gérer la liaison du serveur.
+int handle_bind(char *server_port) {
+    struct addrinfo hints, *result, *rp;
+    int sfd;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC; // Indique que l'on peut utiliser IPv4 ou IPv6.
+    hints.ai_socktype = SOCK_STREAM; // Indique que l'on utilise un socket de type TCP.
+    hints.ai_flags = AI_PASSIVE; // Indique que l'on écoute sur toutes les interfaces.
+
+    // Obtient les informations locales pour la liaison du serveur.
+    if (getaddrinfo(NULL, server_port, &hints, &result) != 0) {
+        perror("getaddrinfo()"); // Affiche un message d'erreur en cas d'échec.
+        exit(EXIT_FAILURE); // Quitte le programme en cas d'erreur.
+    }
+
+    // Parcourt la liste des résultats obtenus à partir de getaddrinfo.
+    for (rp = result; rp != NULL; rp = rp->ai_next) {
+        sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if (sfd == -1) {
+            continue; // Continue la boucle si la création du socket échoue.
+        }
+        if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0) {
+            break; // Établit la liaison du serveur et sort de la boucle.
+        }
+        close(sfd); // Ferme le socket si la liaison échoue.
+    }
+
+    // Vérifie si rp est toujours NULL (liaison échouée).
+    if (rp == NULL) {
+        fprintf(stderr, "Could not bind\n"); // Affiche un message d'erreur.
+        exit(EXIT_FAILURE); // Quitte le programme en cas d'échec de la liaison.
+    }
+
+    freeaddrinfo(result); // Libère la mémoire utilisée par les informations obtenues.
+
+    return sfd; // Retourne le descripteur de socket lié au serveur.
 }
 
-int main() {
-	struct sockaddr cli;
-	int sfd, connfd;
-	socklen_t len;
-	sfd = handle_bind();
-	if ((listen(sfd, SOMAXCONN)) != 0) {
-		perror("listen()\n");
-		exit(EXIT_FAILURE);
-	}
-	len = sizeof(cli);
-	if ((connfd = accept(sfd, (struct sockaddr*) &cli, &len)) < 0) {
-		perror("accept()\n");
-		exit(EXIT_FAILURE);
-	}
-	echo_server(connfd);
-	close(sfd);
-	return EXIT_SUCCESS;
-}
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+        fprintf(stderr, "Usage: %s <server_name> <server_port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
+    char *server_port = argv[1];
+
+    struct sockaddr cli;
+    int sfd, connfd;
+    socklen_t len;
+    sfd = handle_bind(server_port); // Établit la liaison du serveur.
+    if ((listen(sfd, SOMAXCONN)) != 0) {
+        perror("listen()\n"); // Affiche un message d'erreur en cas d'échec.
+        exit(EXIT_FAILURE); // Quitte le programme en cas d'erreur.
+    }
+    len = sizeof(cli);
+    if ((connfd = accept(sfd, (struct sockaddr*) &cli, &len)) < 0) {
+        perror("accept()\n"); // Affiche un message d'erreur en cas d'échec.
+        exit(EXIT_FAILURE); // Quitte le programme en cas d'erreur.
+    }
+    echo_server(connfd); // Lance la fonction de communication serveur-client.
+    close(sfd); // Ferme le socket du serveur.
+    return EXIT_SUCCESS;
+}
