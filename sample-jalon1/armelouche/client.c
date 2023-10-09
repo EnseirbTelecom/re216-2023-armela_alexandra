85a1b9b6960f19a04604eef1cb04aea0c53693fa
Sallmone Armela <asallmone007@bordeaux-inp.fr>
2023-10-09 16:29:49 +0200

    version armelouche

--- sample-jalon1/armelouche/client.c
+++ sample-jalon1/armelouche/client.c
@@ -9,24 +9,35 @@
 
 #include "common.h"
 
+//envoyer des messages au serveur et recevoir des réponses du serveur en bpucle continue 
+
+//sockfd : descripyeur de socket vers le serveur auquel le client est connecté 
 void echo_client(int sockfd) {
+	
 	char buff[MSG_LEN];
 	int n;
 	while (1) {
+		
 		// Cleaning memory
 		memset(buff, 0, MSG_LEN);
-		// Getting message from client
+		
+		// lire le message de l'utilisateur depuis la console
 		printf("Message: ");
 		n = 0;
+		
+		// lire les carcatères saisis par l'utilisateur: les caractères sont sockés dans buff
 		while ((buff[n++] = getchar()) != '\n') {} // trailing '\n' will be sent
-		// Sending message (ECHO)
+		
+		// Sending message (ECHO) : envoi le contenu du buff ua serveur
 		if (send(sockfd, buff, strlen(buff), 0) <= 0) {
 			break;
 		}
 		printf("Message sent!\n");
-		// Cleaning memory
+		
+		// Cleaning memory: pour le préparer à la reception d'une réponse
 		memset(buff, 0, MSG_LEN);
-		// Receiving message
+		
+		// Receiving message : attend la réponse où que la connexion avec les erveur soit fait
 		if (recv(sockfd, buff, MSG_LEN, 0) <= 0) {
 			break;
 		}
@@ -34,13 +45,16 @@ void echo_client(int sockfd) {
 	}
 }
 
-int handle_connect() {
+
+// établir une connexion réseau à un serveur distant en utilisant les sockets 
+
+int handle_connect(const char *server_addr, const char *server_port) {
 	struct addrinfo hints, *result, *rp;
 	int sfd;
 	memset(&hints, 0, sizeof(struct addrinfo));
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
-	if (getaddrinfo(SERV_ADDR, SERV_PORT, &hints, &result) != 0) {
+	if (getaddrinfo(server_addr, server_port, &hints, &result) != 0) {
 		perror("getaddrinfo()");
 		exit(EXIT_FAILURE);
 	}
@@ -62,9 +76,18 @@ int handle_connect() {
 	return sfd;
 }
 
-int main() {
+int main(int argc, char*argv[]) {
+	
+	// verifier les arguments passés en paramètre 
+	if (argc !=3){
+		printf("Arguments invalides !\n");
+	}
+	
+	const char *server_name = argv[1];
+	const char *server_port = argv[2];
+
 	int sfd;
-	sfd = handle_connect();
+	sfd = handle_connect(server_name, server_port);
 	echo_client(sfd);
 	close(sfd);
 	return EXIT_SUCCESS;
