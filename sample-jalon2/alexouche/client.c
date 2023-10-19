65962937502b6e04d1ad0d724af8aee2e4a1a24b
MONY ALEXANDRA <amony001@enseirb-matmeca.fr>
2023-10-19 10:29:03 +0200

    upadate jalon2

--- sample-jalon2/alexouche/client.c
+++ sample-jalon2/alexouche/client.c
@@ -1,64 +1,169 @@
-#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <netdb.h>
 #include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stddef.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <ctype.h>
 
 #include "common.h"
 #include "msg_struct.h"
 
-void echo_client(int sockfd) {
+int containsOnlyAlphanumeric(char str[]) {
+	printf("%lu\n",strlen(str));
+    for (int i = 0; i < strlen(str)-1; i++)
+	{
+		if(!isalpha(str[i])&&!isdigit(str[i]))
+			return 0;
+	}
+	
+    return 1; // Tous les caractères sont des chiffres ou des lettres
+}
+
+int is_order_nick(char* buff,char** my_nickname){
+
+	if (strncmp(buff, "/nick ", strlen("/nick ")) == 0)
+	{
+		char* new_nick = strstr(buff, "/nick ");
+		
+		new_nick += strlen("/nick ");
+		int nick_len=strlen(new_nick);
+
+		if ((nick_len<3)||(nick_len>127)){
+			printf("The my_nickname size is incorrect; it should be between 3 and 127 characters.");
+			return 0;
+		}
+
+
+		if (containsOnlyAlphanumeric(new_nick)==0){
+			printf("The my_nickname should only contain letters of the alphabet or numbers.");
+			return 0;
+		}
+		*my_nickname=new_nick;
+		return 1;		
+	}
+	return 0;
+}
+
+int send_nick(char** my_nickname,struct message msgstruct,int sockfd_server){
+
+	printf("sn : %s\n",*my_nickname);
+	// Filling structure
+	msgstruct.pld_len = 1;
+	strncpy(msgstruct.nick_sender, "\0", 1);
+	msgstruct.type = NICKNAME_NEW ;
+	strncpy(msgstruct.infos, *my_nickname, strlen(*my_nickname));
+	// Sending structure
+	if (send(sockfd_server, &msgstruct, sizeof(msgstruct), 0) <= 0) {
+		perror("send");
+		return 0;
+	}
+	// Sending message (ECHO)
+	if (send(sockfd_server, "/0", msgstruct.pld_len, 0) <= 0) {
+		perror("send");
+		return 0;
+	}
+	printf("Message sent!\n");	
+	return 1;
+}
+
+
+int echo_client(int sockfd_active,int sockfd_server, int sockfd_entree,char** my_nickname) {
 	struct message msgstruct;
 	char buff[MSG_LEN];
 	int n;
-	while (1) {
+
+	if (sockfd_active==sockfd_entree)
+	{
 		// Cleaning memory
 		memset(&msgstruct, 0, sizeof(struct message));
 		memset(buff, 0, MSG_LEN);
 		// Getting message from client
-		printf("Message: ");
 		n = 0;
 		while ((buff[n++] = getchar()) != '\n') {} // trailing '\n' will be sent
+		
+		if (is_order_nick(buff,my_nickname))
+		{
+			if(send_nick(my_nickname,msgstruct,sockfd_server)==0){
+				return 0;
+			}
+		}
+		/*
 		// Filling structure
 		msgstruct.pld_len = strlen(buff);
-		strncpy(msgstruct.nick_sender, "Toto", 4);
+		strncpy(msgstruct.nick_sender, my_nickname, strlen(my_nickname));
 		msgstruct.type = ECHO_SEND;
 		strncpy(msgstruct.infos, "\0", 1);
 		// Sending structure
-		if (send(sockfd, &msgstruct, sizeof(msgstruct), 0) <= 0) {
-			break;
+		if (send(sockfd_server, &msgstruct, sizeof(msgstruct), 0) <= 0) {
+			perror("send");
+			return 0;
 		}
 		// Sending message (ECHO)
-		if (send(sockfd, buff, msgstruct.pld_len, 0) <= 0) {
-			break;
+		if (send(sockfd_server, buff, msgstruct.pld_len, 0) <= 0) {
+			perror("send");
+			return 0;
 		}
 		printf("Message sent!\n");
+		*/
+	}
+	if (sockfd_active==sockfd_server)
+	{
+		printf("server actif\n");
 		// Cleaning memory
 		memset(&msgstruct, 0, sizeof(struct message));
 		memset(buff, 0, MSG_LEN);
+
 		// Receiving structure
-		if (recv(sockfd, &msgstruct, sizeof(struct message), 0) <= 0) {
-			break;
+		if (recv(sockfd_server, &msgstruct, sizeof(struct message), 0) <= 0) {
+			perror("recv");
+			return 0;
 		}
 		// Receiving message
-		if (recv(sockfd, buff, msgstruct.pld_len, 0) <= 0) {
-			break;
+		if (recv(sockfd_server, buff, msgstruct.pld_len, 0) <= 0) {
+			perror("recv");
+			return 0;
 		}
+
 		printf("pld_len: %i / nick_sender: %s / type: %s / infos: %s\n", msgstruct.pld_len, msgstruct.nick_sender, msg_type_str[msgstruct.type], msgstruct.infos);
-		printf("Received: %s", buff);
-	}
+		printf("[Server]: %s \n", buff);
+
+
+
+		if (strcmp(msgstruct.nick_sender, "server") )
+		{
+			printf("[Server]: %s", buff);
+			return 1;
+		}
+		
+		//printf("pld_len: %i / nick_sender: %s / type: %s / infos: %s\n", msgstruct.pld_len, msgstruct.nick_sender, msg_type_str[msgstruct.type], msgstruct.infos);
+		//printf("[Server]: %s", buff);
+
+		/* Demande fermeture de connexion
+		if (strcmp(buff, "/quit\n") == 0) {
+            printf("Server accepted the request to close the connection. Closing...\n");
+            close(sockfd_server);  // Ferme la connexion du client.
+            exit(EXIT_SUCCESS);  // Arrête le programme client.
+        }
+		*/
+	}	
+	return 1;
 }
 
-int handle_connect() {
+int handle_connect(char * server_name,char * server_port) {
 	struct addrinfo hints, *result, *rp;
 	int sfd;
 	memset(&hints, 0, sizeof(struct addrinfo));
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
-	if (getaddrinfo(SERV_ADDR, SERV_PORT, &hints, &result) != 0) {
+	if (getaddrinfo(server_name, server_port, &hints, &result) != 0) {
 		perror("getaddrinfo()");
 		exit(EXIT_FAILURE);
 	}
@@ -80,11 +185,64 @@ int handle_connect() {
 	return sfd;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+	if (argc != 3) {
+        fprintf(stderr, "Usage: %s <server_name> <server_port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+	char * server_name=argv[1];
+	char * server_port=argv[2];
+
 	int sfd;
-	sfd = handle_connect();
-	printf("Connecting to server ... done!");
-	echo_client(sfd);
+	sfd = handle_connect(server_name,server_port);
+	printf("Connecting to server ... done!\n");
+	
+
+    // Initialisation du tableau de structures pollfd
+    int max_conn = 2;
+    struct pollfd fds[max_conn];
+    memset(fds, 0, max_conn * sizeof(struct pollfd));
+
+    // Insertion de sfd dans le tableau
+    fds[0].fd = sfd;
+    fds[0].events = POLLIN;
+
+	// Insertion de l'entrée standars dans le tableau
+    fds[1].fd = fileno(stdin);
+    fds[1].events = POLLIN;
+
+	int ret;
+	char *my_nickname=malloc(NICK_LEN*sizeof(char));
+
+	while (1) {
+        // Appel à poll pour attendre de nouveaux événements
+        int active_fds = poll(fds, max_conn, -1);
+		if (active_fds<0){
+			perror("poll");
+            exit(EXIT_FAILURE);
+		}
+		
+
+		//si il y a de l'activité sur la socket liée au serveur
+		if ((fds[0].revents & POLLIN) == POLLIN) {
+			ret=echo_client(fds[0].fd,fds[0].fd,fds[1].fd,&my_nickname);
+			if (ret==0){
+				perror("echo_client");
+				exit(EXIT_FAILURE);
+			}
+			
+		}
+
+		//si il y a de l'activité sur la socket de l'entrée standard
+		if ((fds[1].revents & POLLIN) == POLLIN) {
+			ret=echo_client(fds[1].fd,fds[0].fd,fds[1].fd,&my_nickname);
+			if (ret==0){
+				perror("echo_client");
+				exit(EXIT_FAILURE);
+			}
+		}
+	 }
 	close(sfd);
 	return EXIT_SUCCESS;
 }
--- sample-jalon2/alexouche/server.c
+++ sample-jalon2/alexouche/server.c
@@ -1,45 +1,210 @@
-#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <netdb.h>
 #include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stddef.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <poll.h>
 
 #include "common.h"
 #include "msg_struct.h"
 
-void echo_server(int sockfd) {
+struct Client {
+    int sockfd;
+	char* nickname;
+    struct in_addr addr;
+	unsigned short int port;
+    struct Client * next;
+};
+
+int ask_nickname(char * my_nickname,int sockfd){
 	struct message msgstruct;
-	char buff[MSG_LEN];
-	while (1) {
-		// Cleaning memory
-		memset(&msgstruct, 0, sizeof(struct message));
-		memset(buff, 0, MSG_LEN);
-		// Receiving structure
-		if (recv(sockfd, &msgstruct, sizeof(struct message), 0) <= 0) {
-			break;
-		}
-		// Receiving message
-		if (recv(sockfd, buff, msgstruct.pld_len, 0) <= 0) {
-			break;
-		}
-		printf("pld_len: %i / nick_sender: %s / type: %s / infos: %s\n", msgstruct.pld_len, msgstruct.nick_sender, msg_type_str[msgstruct.type], msgstruct.infos);
-		printf("Received: %s", buff);
-		// Sending structure (ECHO)
-		if (send(sockfd, &msgstruct, sizeof(msgstruct), 0) <= 0) {
-			break;
-		}
-		// Sending message (ECHO)
-		if (send(sockfd, buff, msgstruct.pld_len, 0) <= 0) {
-			break;
-		}
-		printf("Message sent!\n");
+	char buff[MSG_LEN]="please login with /nick <your pseudo>";
+	// Filling structure
+	msgstruct.pld_len = strlen(buff);
+	strncpy(msgstruct.nick_sender, my_nickname, strlen(my_nickname));
+	msgstruct.type = NICKNAME_NEW;
+	strncpy(msgstruct.infos, "\0", 1);
+
+	// Sending structure
+	if (send(sockfd, &msgstruct, sizeof(msgstruct), 0) <= 0) {
+		perror("send");
+		return 0;
 	}
+	// Sending message (ECHO)
+	if (send(sockfd, buff, msgstruct.pld_len, 0) <= 0) {
+		perror("send");
+		return 0;
+	}	
+	printf("Ask nickname send\n");
+	return 1;
 }
 
-int handle_bind() {
+int nickname_already_attribuate(char * my_nickname,int sockfd){
+	struct message msgstruct;
+	char buff[MSG_LEN]="Nickname already attribuate";
+	// Filling structure
+	msgstruct.pld_len = strlen(buff);
+	strncpy(msgstruct.nick_sender, my_nickname, strlen(my_nickname));
+	msgstruct.type = NICKNAME_NEW;
+	strncpy(msgstruct.infos, "\0", 1);
+
+	// Sending structure
+	if (send(sockfd, &msgstruct, sizeof(msgstruct), 0) <= 0) {
+		perror("send");
+		return 0;
+	}
+	// Sending message (ECHO)
+	if (send(sockfd, buff, msgstruct.pld_len, 0) <= 0) {
+		perror("send");
+		return 0;
+	}	
+	printf("send\n");
+	return 1;
+}
+
+
+
+/*
+int echo_server(int* num_clients,struct pollfd * fds,int i) {
+	char buff[MSG_LEN];
+	int sockfd=fds[i].fd;
+	
+	// Cleaning memory
+	memset(buff, 0, MSG_LEN);
+	// Receiving message
+	if ((recv(sockfd, buff, MSG_LEN, 0) <= 0)) {
+		*num_clients=*num_clients-1;
+		printf("Client disconnected. Total clients: %d\n", *num_clients);
+		close(sockfd);
+		fds[i].fd=0;
+		return(-1);
+		perror("recv");
+		return(-1);
+	}
+
+	printf("Received: %s", buff);
+
+	//If message = "/quit"
+	if (strcmp(buff, "/quit\n") == 0) {
+		printf("Client requested to close the connection. Closing...\n");
+		
+		// Sending closing message (ECHO)
+		if (send(sockfd, buff, strlen(buff), 0) <= 0) {
+			perror("send");
+			return(-1);
+		}
+
+		printf("Closing message sent!\n");
+		
+		*num_clients=*num_clients-1;
+		printf("Client disconnected. Total clients: %d\n", *num_clients);
+		close(sockfd);
+		fds[i].fd=0;
+        fds[i].events=0;
+        fds[i].revents=0;
+		return(0);
+	}
+
+	// Sending message (ECHO)
+	if (send(sockfd, buff, strlen(buff), 0) <= 0) {
+		perror("send");
+		return(-1);
+	}
+
+	printf("Message sent!\n");
+	return(1);
+		
+}
+*/
+
+int echo_server(int* num_clients,struct pollfd * fds,int i,char **nick_client,char** clients_connected,struct Client * chaine_cli_head) {
+	struct message msgstruct;
+	char buff[MSG_LEN];
+
+	int sockfd=fds[i].fd;
+	
+
+	// Cleaning memory
+	memset(&msgstruct, 0, sizeof(struct message));
+	memset(buff, 0, MSG_LEN);
+
+	// Receiving structure
+	if (recv(sockfd, &msgstruct, sizeof(struct message), 0) <= 0) {
+		perror("recv");
+		printf("Client disconnected. Total clients: %d\n", *num_clients);
+		close(sockfd);
+		fds[i].fd=0;
+		return(0);
+	}
+	// Receiving message
+	if (recv(sockfd, buff, msgstruct.pld_len, 0) <= 0) {
+		perror("recv");
+		return 0;
+	}
+
+	
+
+
+	if (msgstruct.type ==NICKNAME_NEW )
+	{
+		printf("Message type nickname\n");
+		for (int i = 0; i < 128; i++) {
+			if (strcmp(msgstruct.infos, clients_connected[i]) == 0) {
+				int ret = nickname_already_attribuate("server",sockfd);
+				if (ret==0){
+					return 0;
+				}
+				return 1; // La chaîne est trouvée dans le tableau
+			}
+		}
+
+		// On met à jour dans la liste des clients connectés
+		*nick_client=msgstruct.infos;
+		printf("Nickname updated in connected client list\n");
+	
+		
+		
+		//OPn met à jour dans la liste des clients
+		struct Client * find_cli = chaine_cli_head;
+			
+		while (find_cli->next!=NULL){
+			if ((find_cli->nickname==msgstruct.nick_sender)&&(find_cli->sockfd==sockfd)){
+				find_cli->nickname=msgstruct.infos;
+				printf("Nickname updated in client linked list\n");
+				break;
+			}
+			else 
+				find_cli=find_cli->next;
+		}
+	}
+	
+	printf("pld_len: %i / nick_sender: %s / type: %s / infos: %s\n", msgstruct.pld_len, msgstruct.nick_sender, msg_type_str[msgstruct.type], msgstruct.infos);
+	printf("Received: %s", buff);
+	printf(" hello %s \n",msg_type_str[msgstruct.type]);
+	// Sending structure (ECHO)
+	if (send(sockfd, &msgstruct, sizeof(msgstruct), 0) <= 0) {
+		perror("send");
+		return 0;
+	}
+	// Sending message (ECHO)
+	if (send(sockfd, buff, msgstruct.pld_len, 0) <= 0) {
+		perror("send");
+		return 0;
+	}
+	printf("Message sent!\n");
+	return 1;
+}
+	
+
+
+int handle_bind(char* server_port)  {
 	struct addrinfo hints, *result, *rp;
 	int sfd;
 	memset(&hints, 0, sizeof(struct addrinfo));
@@ -69,22 +234,141 @@ int handle_bind() {
 	return sfd;
 }
 
-int main() {
-	struct sockaddr cli;
-	int sfd, connfd;
-	socklen_t len;
-	sfd = handle_bind();
-	if ((listen(sfd, SOMAXCONN)) != 0) {
-		perror("listen()\n");
-		exit(EXIT_FAILURE);
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <server_port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+	char *server_port = argv[1];
+
+    int sfd = handle_bind(server_port);
+
+    if ((listen(sfd, SOMAXCONN)) != 0) {
+        perror("listen()\n");
+        exit(EXIT_FAILURE);
+    }
+
+	//initialisation du nombre de client
+	int num_clients=0;
+
+    // Initialisation du tableau de structures pollfd
+    int max_conn = 256;
+    struct pollfd fds[max_conn];
+    memset(fds, 0, max_conn * sizeof(struct pollfd));
+
+    // Insertion de listen_fd dans le tableau
+    fds[0].fd = sfd;
+    fds[0].events = POLLIN;
+
+	char my_nickname[NICK_LEN]="server";
+	char **clients_connected=malloc(max_conn*sizeof(int*));
+	for (int i = 0; i < max_conn; i++)
+	{
+		clients_connected[i]=malloc(NICK_LEN*sizeof(int));
 	}
-	len = sizeof(cli);
-	if ((connfd = accept(sfd, (struct sockaddr*) &cli, &len)) < 0) {
-		perror("accept()\n");
-		exit(EXIT_FAILURE);
-	}
-	echo_server(connfd);
-	close(sfd);
-	return EXIT_SUCCESS;
+	
+
+	struct Client * chaine_cli = NULL;
+	struct Client * chaine_cli_head = NULL;
+	while (1) {
+        // Appel à poll pour attendre de nouveaux événements
+        int active_fds = poll(fds, max_conn, -1);
+        if (active_fds<0){
+			perror("poll");
+            exit(EXIT_FAILURE);
+		}
+
+        for (int i = 0; i < max_conn; i++) {
+            // S'il y a de l'activité sur fds, accepter une nouvelle connexion
+            if (fds[i].fd == sfd && (fds[i].revents & POLLIN) == POLLIN) {
+                
+                // Accepter la nouvelle connexion et ajouter le nouveau descripteur de fichier dans le tableau fds
+                struct sockaddr_in client_addr;
+                socklen_t len = sizeof(client_addr);
+                int new_fd = accept(sfd, (struct sockaddr *)&client_addr, &len);
+                if (new_fd==-1){
+                    perror("accept");
+                    exit(EXIT_FAILURE);
+                }
+				num_clients++;
+
+				// Obtention de l'adresse IP du client
+				char client_ip[INET_ADDRSTRLEN];
+				if (inet_ntop(AF_INET, &(client_addr.sin_addr), client_ip, INET_ADDRSTRLEN) == NULL) {
+					perror("inet_ntop");
+					exit(EXIT_FAILURE);
+				}
+
+				// Obtention du port du client
+				int client_port = ntohs(client_addr.sin_port);
+
+				//Création de la nouvelle structure client 
+				struct Client *new_client=malloc(sizeof(struct Client));
+				new_client->sockfd=new_fd;
+				new_client->addr=client_addr.sin_addr;
+				new_client->port=client_addr.sin_port;
+				new_client->nickname="/0";
+                new_client->next=NULL;
+
+				ask_nickname(my_nickname,new_fd);
+
+                //Cas premier client
+				if (chaine_cli==NULL){
+					chaine_cli=new_client;
+					chaine_cli_head=new_client;
+				}
+                //Sinon on décale.
+				else{
+					chaine_cli->next=new_client;
+                    chaine_cli=new_client;
+				}
+
+				printf("New connection from %s:%d (client n°%d) on socket %d\n", client_ip, client_port,num_clients,new_fd);
+
+                for (int j = 0; j < max_conn; j++) {
+                    if (fds[j].fd == 0) {
+                        fds[j].fd = new_fd;
+                        fds[j].events = POLLIN;
+						fds[j].revents = 0;
+						break;
+                    }
+                }
+
+				fds[i].revents = 0;
+            }
+
+            // S'il y a de l'activité sur un socket autre que listen_fd, lire les données
+            if (fds[i].fd != sfd && (fds[i].revents & POLLIN) == POLLIN) {
+                printf("Activity on socket %d\n", fds[i].fd);
+				if (strcmp(clients_connected[i],"/0"))
+				{
+					ask_nickname(my_nickname,fds[i].fd);
+				}
+				
+                int ret=echo_server(&num_clients,fds,i,&clients_connected[i],clients_connected,chaine_cli_head)  ;
+                if (ret==-1){
+                    perror("echo_server");
+                    exit(EXIT_FAILURE);
+                }
+            }
+        }
+    }
+
+    close(sfd);
+    return EXIT_SUCCESS;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
